0c56fd1f3f92ca91fee097e223b718ece7738c72
Hakim Hauston <hakimh@google.com>
2023-11-20 15:43:44 +0800

    [adpf] audio - added some bgm and audio effect

--- NewProject/assets/HelloWorld.ts
+++ NewProject/assets/HelloWorld.ts
@@ -1,8 +1,14 @@
-import { _decorator, Component, Node, native } from 'cc';
+import { _decorator, assert, AudioSource, Component, Node, native } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('HelloWorld')
 export class HelloWorld extends Component {
+
+    public _audioSource: AudioSource = null!
+
+    @property(AudioSource)
+    bgmSource: AudioSource = null;
+
     start() {
         // console.info('thermalHeadroom: ' + native.adpf.thermalHeadroom);
         console.info('thermalHeadroom: ' + native); // [Object object]
@@ -10,11 +16,37 @@ export class HelloWorld extends Component {
         console.info(native.adpf);
         // PROP_MIN_SDK_VERSION=31
         console.info('thermalHeadroom: ' + JSON.stringify(native)); // "adpf":{"thermalHeadroom":0.7152467370033264,"thermalStatus":0,"thermalStatusMin":0,"thermalStatusMax":6,"thermalStatusNormalized":0}
+
+        if ( native.adpf ) {
+            console.info('thermalHeadroom: ' + JSON.stringify(native.adpf)); // "adpf":{"thermalHeadroom":0.7152467370033264,"thermalStatus":0,"thermalStatusMin":0,"thermalStatusMax":6,"thermalStatusNormalized":0}
+        }
     }
 
     update(deltaTime: number) {
         if ( native.adpf ) {
-            console.info('thermalHeadroom: ' + JSON.stringify(native.adpf)); // "adpf":{"thermalHeadroom":0.7152467370033264,"thermalStatus":0,"thermalStatusMin":0,"thermalStatusMax":6,"thermalStatusNormalized":0}
+            // console.info('thermalHeadroom: ' + JSON.stringify(native.adpf)); // "adpf":{"thermalHeadroom":0.7152467370033264,"thermalStatus":0,"thermalStatusMin":0,"thermalStatusMax":6,"thermalStatusNormalized":0}
+        }
+    }
+
+    buttonClickPlayWave() {
+        console.info('HelloWorld.buttonClickPlayWave');
+        const audioSource = this.node.getComponent(AudioSource);
+        assert(audioSource);
+        this._audioSource = audioSource;
+        this._audioSource.play();
+
+        if ( this.bgmSource != null ) {
+            this.bgmSource.play();
+        }
+    }
+
+    buttonClickPause() {
+        console.info('HelloWorld.buttonClickPause');
+        if ( this._audioSource != null ) {
+            this._audioSource.pause();
+        }
+        if ( this.bgmSource != null ) {
+            this.bgmSource.pause();
         }
     }
 }
